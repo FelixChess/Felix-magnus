b9e6aba733b5f687025a783b2330df4e52468c5c
Sebastian <sebh@tdc.dk>
2021-04-12 22:22:01 +0200

    super-kills

--- counter.java
+++ counter.java
@@ -2,7 +2,7 @@ public class counter {
     public static void main (String[] args) { 
         int h = 5;
         int g = 5;
-        String z = "  mine final-kills";
+        String z = "  mine final-super-kills";
         String y = " er mængden af";
         double x = 0.4576;
         while ( h == g ) {
