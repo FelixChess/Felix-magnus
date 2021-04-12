62b43b1f116d3378c224e3c960f5a723b9ba9a7d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-12 22:23:53 +0200

    sdfghjk

--- counter.java
+++ counter.java
@@ -3,7 +3,7 @@ public class counter {
         int h = 5;
         int g = 5;
         String z = "  mine final-kills";
-        String y = " er mængden af";
+        String y = " er mængden af alle";
         double x = 0.4576;
         while ( h == g ) {
             x = x + 11.56237656;
