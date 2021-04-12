e9e8663646e02a50753ecb9b1d569b03fe441a4b
BuildTools <unconfigured@null.spigotmc.org>
2021-04-12 20:46:01 +0200

    test commit

---
+++ clock.java
@@ -0,0 +1,12 @@
+public class clock {
+    public static void main (String[] args) { 
+        
+      <html>
+      <body>
+      
+      <h1>
+
+
+
+    }
+}       s
---
+++ counter.java
@@ -0,0 +1,17 @@
+public class counter {
+    public static void main (String[] args) { 
+        int h = 5;
+        int g = 5;
+        String z = "  mine final-kills";
+        String y = " er mængden af";
+        double x = 0.4576;
+        while ( h == g ) {
+            x = x + 11.56237656;
+               
+            System.out.println (x + y + z);           
+            
+        }
+    }
+}    
+
+
